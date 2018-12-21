a3e66487f567d4874409b501a5e842c252b62d8a
tim finch <tim.finch@rocketmail.com>
2018-12-21 16:04:28 +0000

    sprint1

---
+++ cdisc/Scripts/page2.js
@@ -0,0 +1,16 @@
+﻿/*
+ * Page2.js
+ */
+
+'use strict';
+
+document.addEventListener("keyup", function (e) {
+    var key = e.which || e.keyCode;
+    switch (key) {
+        // the Del key
+        case 46:
+            window.location.replace("page3.html");
+            break;
+        
+    }
+});
---
+++ cdisc/Scripts/page4.js
@@ -0,0 +1,19 @@
+﻿/*
+ * Page4.js
+ */
+
+'use strict';
+
+document.addEventListener("keyup", function (e) {
+    var key = e.which || e.keyCode;
+    switch (key) {
+        //left arrow
+        case 37:
+            window.location.replace("page3.html");
+            break;
+        //right arrow
+        case 39:
+            window.location.replace("page5.html");
+            break;
+    }
+});
